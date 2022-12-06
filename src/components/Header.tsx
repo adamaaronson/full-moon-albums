6ac822825b5c6b5be2cafc7d07da680919d99e1f
Adam Aaronson <adamaaronson@gmail.com>
2022-12-05 20:57:42 -0600

    Get moonburst animation working

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,33 @@ import "../css/Header.scss"
 export function Header() {
     return <div className="header">
         <div className="moon-image">
-            <img src="fullmoon.svg" className="full-moon-emoji"></img>
-            <img src="moonburst-dark.svg" className="moonburst"></img>
+            <div className="rotator-wrapper moon-wrapper">
+                <div className="rotator moon-rotator">
+                    <img src="fullmoon.svg" className="full-moon-emoji"></img>
+                </div>
+            </div>
+            <div className="rotator-wrapper moonburst-wrapper moonburst-dark-wrapper">
+                <div className="rotator">
+                    <img src="moonburst-dark.svg" className="moonburst moonburst-dark"></img>
+                </div>
+            </div>
+            <div className="rotator-wrapper moonburst-wrapper moonburst-bright-wrapper">
+                <div className="rotator">
+                    <img src="moonburst-bright.svg" className="moonburst moonburst-bright"></img>
+                </div>
+            </div>
         </div>
-        <h1 className="header-title">
-            Full<br/>
-            Moon<br/>
-            Albums
-        </h1>
+        <div className="header-title-wrapper">
+            <svg className="header-title-svg">
+                <clipPath id="title-clip">
+                    <text x="0" y="0" dy="0" className="title-text">
+                        <tspan x="0" dy="-1em" className="header-title" dominantBaseline="central">Full </tspan>
+                        <tspan x="0" dy="1em" className="header-title" dominantBaseline="central">Moon </tspan>
+                        <tspan x="0" dy="1em" className="header-title" dominantBaseline="central">Albums</tspan>
+                    </text>
+                </clipPath>
+            </svg>
+        </div>
+            
     </div>
 }
