5b6cda58b8bfea19b8347cc3c122a7dcf4dfa3f9
Adam Aaronson <adamaaronson@gmail.com>
2023-05-09 12:41:19 -0500

    Make footer cleaner

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,12 @@ export function Footer() {
     return <div className="app-footer-text">
         © {new Date().getFullYear()} <a href="https://aaronson.org" rel="noreferrer noopener" target="_blank" className="hover-blue">
             Adam Aaronson
-        </a> · See the <a href="https://github.com/adamaaronson/full-moon-albums" rel="noreferrer noopener" target="_blank" className="hover-blue">
-            source code
-        </a> · Got recs? <a href="https://twitter.com/aaaronson" rel="noreferrer noopener" target="_blank" className="hover-blue">
-            Message me
-        </a>!
+        </a> · <a href="https://github.com/adamaaronson/full-moon-albums" rel="noreferrer noopener" target="_blank" className="hover-blue">
+            GitHub
+        </a> · <a href="https://twitter.com/aaaronson" rel="noreferrer noopener" target="_blank" className="hover-blue">
+            Twitter
+        </a> · <a href="https://aaronson.org/blog/full-moon-albums" rel="noreferrer noopener" target="_blank" className="hover-blue">
+            Learn more
+        </a>
     </div>
 }
