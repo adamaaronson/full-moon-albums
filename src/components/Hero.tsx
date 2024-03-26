db78a7255583c4f7f3837cbddbb40ba97ddc4b0c
Adam Aaronson <adamaaronson@gmail.com>
2024-03-26 19:26:35 -0400

    Update script and number

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -110,7 +110,7 @@ export function Hero() {
                     </li>
                 </ul>
                 <p>
-                    After rating <strong>1,800+</strong> albums, these are the
+                    After rating <strong>1,900+</strong> albums, these are the
                     only ones that slap from start to finish, the ones I keep
                     coming back to, the ones that earned themselves a{' '}
                     <strong>Full Moon</strong>.{' '}
--- update-albums.sh
+++ update-albums.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd src/data
 
 python3 process-albums.py
