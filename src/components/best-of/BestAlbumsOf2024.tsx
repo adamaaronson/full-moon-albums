bc955dbbbaa7577fba1e7e3e3f0517087d44c7e7
Adam Aaronson <adamaaronson@gmail.com>
2024-12-15 13:23:19 -0500

    Use React Helmet and add social card for album list

--- src/components/best-of/BestAlbumsOf2024.tsx
+++ src/components/best-of/BestAlbumsOf2024.tsx
@@ -3,14 +3,42 @@ import bestAlbumsOf2024 from '../../data/best-of/best-albums-of-2024.json';
 import { AlbumGrid } from '../AlbumGrid';
 import '../../css/BestOf.scss';
 import { Album } from '../Album';
+import { Helmet } from 'react-helmet';
 
 export default function BestAlbumsOf2024() {
-    useEffect(() => {
-        document.title = 'Best Albums of 2024';
-    }, []);
-
     return (
         <div className="app best-albums-of-year">
+            <Helmet>
+                <title>Best Albums of 2024</title>
+                <meta
+                    name="description"
+                    content="I listened to over 250 albums that came out this year. These are my favorites."
+                />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@aaaronson" />
+                <meta name="twitter:title" content="Best Albums of 2024" />
+                <meta
+                    name="twitter:description"
+                    content="I listened to over 250 albums that came out this year. These are my favorites."
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://aaronson.org/full-moon-albums/best-albums-of-2024-card.png"
+                />
+                <meta property="og:title" content="Best Albums of 2024" />
+                <meta
+                    property="og:url"
+                    content="https://aaronson.org/full-moon-albums/#/best-albums-of-2024/"
+                />
+                <meta
+                    property="og:description"
+                    content="I listened to over 250 albums that came out this year. These are my favorites."
+                />
+                <meta
+                    property="og:image"
+                    content="https://aaronson.org/full-moon-albums/best-albums-of-2024-card.png"
+                />
+            </Helmet>
             <div className="pattern"></div>
             <header className="best-albums-header">
                 <div className="best-albums-header-contents">
